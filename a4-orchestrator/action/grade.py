24a9486ee6f3b0d2763fb2512fe4ad3e3ea420e8
Aditya Saligrama <aditya@saligrama.io>
2024-03-08 18:32:46 -0800

    Add a4-orchestrator code

---
+++ a4-orchestrator/action/grade.py
@@ -0,0 +1,124 @@
+import json
+import re
+import sys
+from typing import List
+
+from jinja2 import Template
+import yaml
+
+
+class CustomEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, GradescopeTest):
+            return obj.__dict__
+        return super().default(obj)
+
+
+class GradescopeTest:
+    def __init__(
+        self,
+        name: str,
+        score: int = 0,
+        max_score: int = 10,
+        failure: str = "Failed",
+    ):
+        self.name: str = name
+        self.score: int = score
+        self.max_score: int = max_score
+        self.output: str = failure
+
+    def mark_passed(self):
+        self.output = "Pass"
+        self.score = self.max_score
+
+
+TESTS: List[GradescopeTest] = [
+    GradescopeTest("Validate step 6 does not push built images"),
+    GradescopeTest("Validate step 6 tags are not malformed"),
+    GradescopeTest("Validate step 7 latest image is properly removed"),
+    GradescopeTest("Validate step 8 does not push the built images"),
+    GradescopeTest("Validate step 8 tags are not malformed"),
+]
+
+
+def finalize_grades() -> None:
+    final_grade = sum([test.score for test in TESTS])
+    print(
+        json.dumps(
+            {"actions_grade": final_grade, "results": TESTS},
+            cls=CustomEncoder,
+            indent=2,
+        )
+    )
+    exit(0)
+
+
+# Read YAML content from file
+yaml_content = open(sys.argv[1], "r").read()
+
+# Define hardcoded env variables
+values = {
+    "secrets": {"AWS_ACCOUNT_ID": "123456789123"},
+    "env": {
+        "AWS_REGION": "us-west-2",
+        "ECR_REGISTRY": "123456789123.dkr.ecr.us-west-2.amazonaws.com",
+        "ECR_REPOSITORY": "cs40",
+    },
+    "steps": {"timestamp": {"outputs": {"timestamp": "1709649832"}}},
+}
+
+if __name__ == "__main__":
+    # Rendering the template
+    template = Template(yaml_content)
+    rendered_yaml = template.render(**values)
+
+    # for the people who hardcoded stuff and didn't reference the environment variables
+    rendered_yaml = re.sub(r"\b\d{12}\b", "123456789123", rendered_yaml).replace(
+        "$", ""
+    )
+
+    steps = yaml.safe_load(rendered_yaml)["jobs"]["build-and-push"]["steps"]
+
+    if steps[5]["with"]["push"] != False:
+        TESTS[0].output = "Step 6 should not push the built images"
+    else:
+        TESTS[0].mark_passed()
+
+    tags = re.split("\n|,", steps[5]["with"]["tags"])
+    if len(tags) > 0 and tags[0][0] == "-":
+        tags = [tag[1:] for tag in tags]
+    tags = [tag.strip() for tag in tags if tag]
+
+    if (
+        len(tags) != 2
+        or "123456789123.dkr.ecr.us-west-2.amazonaws.com/cs40:latest" not in tags
+        or not "123456789123.dkr.ecr.us-west-2.amazonaws.com/cs40:1709649832" in tags
+    ):
+        TESTS[1].output = "Step 6 tags are malformed"
+    else:
+        TESTS[1].mark_passed()
+
+    if (
+        not steps[6]["run"].strip().startswith("aws ecr batch-delete-image")
+        or "imageTag=latest" not in steps[6]["run"]
+    ):
+        TESTS[2].output = "Step 7 does not remove the latest image properly"
+    else:
+        TESTS[2].mark_passed()
+
+    if "push" in steps[7]["with"] and steps[7]["with"]["push"] != True:
+        TESTS[3].output = "Step 8 should push the built images"
+    else:
+        TESTS[3].mark_passed()
+
+    tags = steps[7]["with"]["tags"].split(",")
+    if (
+        len(tags) != 2
+        or "123456789123.dkr.ecr.us-west-2.amazonaws.com/cs40:latest" not in tags
+        or not "123456789123.dkr.ecr.us-west-2.amazonaws.com/cs40:1709649832" in tags
+    ):
+        TESTS[4].output = "Step 8 tags are malformed"
+    else:
+        TESTS[4].mark_passed()
+
+    finalize_grades()
---
+++ a4-orchestrator/main.go
@@ -0,0 +1,217 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/exec"
+
+	"github.com/go-git/go-git/v5"
+	"github.com/mholt/archiver/v4"
+	"github.com/open-policy-agent/opa/rego"
+)
+
+const LAMBDA_GATEWAY_URI = "https://5tvpsbxptgyc6m7ffmgmxvdw7m0pbmkb.lambda-url.us-east-1.on.aws/"
+const GRADER_TOKEN = "9007b79f4f9927ea8479076f4fdbec6aca26420558ba0a2fa78c2438d6a5101b"
+
+func makeSubmissionZip() ([]byte, error) {
+	err := os.Chdir("/autograder/submission")
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+
+	// Make directories that would otherwise be Git submodules, but not included by Gradescope submission
+	err = os.MkdirAll("web/dist", 0777)
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+
+	_, err = git.PlainClone("/autograder/submission/app", false, &git.CloneOptions{
+		URL: "https://github.com/infracourse/yoctogram-app.git",
+	})
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+
+	files, err := archiver.FilesFromDisk(nil, map[string]string{
+		".": "",
+	})
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+
+	buf := &bytes.Buffer{}
+	format := archiver.Zip{}
+	err = format.Archive(context.TODO(), buf, files)
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+
+	return buf.Bytes(), nil
+}
+
+type GradingLambdaRequest struct {
+	File []byte
+}
+
+func getCfnResources(gradingLambdaURI string, submissionZip []byte) (map[string]interface{}, error) {
+	request := GradingLambdaRequest{File: submissionZip}
+
+	buf := &bytes.Buffer{}
+	err := json.NewEncoder(buf).Encode(request)
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+
+	resp, err := http.Post(gradingLambdaURI, "application/json", buf)
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+	if resp.StatusCode != http.StatusOK {
+		log.Println("HTTP status code", resp.StatusCode)
+		return nil, fmt.Errorf("synthesizer lambda returned HTTP status code %d", resp.StatusCode)
+	}
+	defer resp.Body.Close()
+
+	var resources map[string]interface{}
+	err = json.NewDecoder(resp.Body).Decode(&resources)
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+
+	return resources, nil
+}
+
+func getOpaEvaluator() (func(r *rego.Rego), error) {
+	_, err := git.PlainClone("/grader", false, &git.CloneOptions{
+		URL: "https://github.com/infracourse/iac-grader.git",
+	})
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+
+	return rego.LoadBundle("/grader/a4-rules"), nil
+}
+
+type GradescopeTest struct {
+	Score    float64 `json:"score"`
+	MaxScore float64 `json:"max_score"`
+	Name     string  `json:"name"`
+	Output   string  `json:"output"`
+}
+
+type ActionCheckOutput struct {
+	ActionsGrade int              `json:"actions_grade"`
+	Results      []GradescopeTest `json:"results"`
+}
+
+func doActionsCheck() (ActionCheckOutput, error) {
+	cmd := exec.Command("python3", "/autograder/action/grade.py", "/autograder/submission/main.yml")
+	if cmd.Err != nil {
+		log.Println(cmd.Err)
+		return ActionCheckOutput{}, cmd.Err
+	}
+
+	var stdout bytes.Buffer
+	cmd.Stdout = &stdout
+
+	if err := cmd.Run(); err != nil {
+		log.Println(err)
+		return ActionCheckOutput{}, err
+	}
+
+	var results ActionCheckOutput
+	err := json.Unmarshal(stdout.Bytes(), &results)
+	if err != nil {
+		log.Println(err)
+		return results, err
+	}
+
+	return results, nil
+}
+
+type GradescopeOutput struct {
+	Score float64          `json:"score"`
+	Tests []GradescopeTest `json:"tests"`
+}
+
+func main() {
+	submissionZip, err := makeSubmissionZip()
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	resources, err := getCfnResources(LAMBDA_GATEWAY_URI, submissionZip)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	evaluator, err := getOpaEvaluator()
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	query, err := rego.New(
+		evaluator,
+		rego.Query("data.rules.main"),
+	).PrepareForEval(context.TODO())
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	results, err := query.Eval(context.TODO(), rego.EvalInput(resources))
+	if err != nil || len(results) == 0 {
+		log.Println(err)
+		return
+	}
+
+	failures := results[0].Expressions[0].Value.(map[string]interface{})["violations"].([]interface{})
+
+	actionsResults, err := doActionsCheck()
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	gradescopeFormattedOutput := GradescopeOutput{
+		Score: 150.0 - (10.0 * float64(len(failures))) - (50.0 - float64(actionsResults.ActionsGrade)),
+		Tests: make([]GradescopeTest, 0, len(failures)+len(actionsResults.Results)),
+	}
+
+	for _, failure := range failures {
+		gradescopeFormattedOutput.Tests = append(
+			gradescopeFormattedOutput.Tests,
+			GradescopeTest{
+				Score:    0,
+				MaxScore: 10.0,
+				Name:     fmt.Sprintf("%v", failure),
+			},
+		)
+	}
+
+	gradescopeFormattedOutput.Tests = append(gradescopeFormattedOutput.Tests, actionsResults.Results...)
+
+	output, err := json.MarshalIndent(gradescopeFormattedOutput, "", "  ")
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	_ = os.WriteFile("/autograder/results/results.json", output, 0777)
+}
